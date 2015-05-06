6acc6725b68cbceb295e611257783a1691adfd1b
Mark Gohara <mgohara@newtek.com>
2015-05-06 08:21:32 -0500

    Created Nav Grid options

---
+++ src/MvcJqGrid/NavGridButtons.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MvcJqGrid
+{
+    public class NavGridButtons
+    {
+        public bool Edit { get; set; }
+        public bool Add { get; set; }
+        public bool Delete { get; set; }
+        public bool Search { get; set; }
+        public bool Refresh { get; set; }
+    }
+}
